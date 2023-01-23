4883889e5def63932dd360f6b172ccfd22d86097
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2023-01-23 16:30:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def get_playlist(playlists):
         
         for video in playlist_urls.videos[:29]:
             
-            titles.append(video.vid_info['videoDetails'])
+            titles.append(video.title)
 
     
     
