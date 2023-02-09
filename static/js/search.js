009080c36a17b9d191819cc659731945576eeed0
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2023-02-09 23:43:50 +0900

    검색기능 완성

--- static/js/search.js
+++ static/js/search.js
@@ -6,7 +6,33 @@ function search(keyword) {
             "keyword": keyword
         },
         success: function (response) {
-            console.log(response)
+            $(`#l_container`).empty()
+            response.forEach(res => {
+                $.ajax({
+                    type: "POST",
+                    url: "/api/musicinfo",
+                    data: {
+                        "id": res.id
+                    },
+                    success: function (res) {
+                        let card = `<div class="list_content" onclick="play('${res.vid}')">
+                                        <div class="list_img">
+                                            <img src="${res.thumbnail}" alt="">
+                                        </div>
+                                        <div class="list_info">
+                                            <p>${res.title}</p>
+                                            <p>${res.author}</p>
+                                        </div>
+                                    </div>`
+                        
+                        //`<li><img onclick="play('${res.vid}')" id="best_img" src="${res.thumbnail}" alt="${res.title}"></li>`
+                        $(`#l_container`).append(card)
+                    }})
+            });
         }
     })
+}
+
+function play(code) {
+    window.top.play(code)
 }
